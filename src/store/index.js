f8c1d8bf8479d6e1e254309dfd618676fa0ac41e
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-04 15:38:52 +0200

    redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,12 @@
-import { createStore } from "redux";
-const counterReducer = (state = { counter: 0, showCounter: false }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.payload,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
+import {configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counter'
+import authReducer from './auth'
 
-  if (action.type === "toggle"){
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer
     }
-  }
-     return state;
-};
-const store = createStore(counterReducer);
+})
 
-export default store;
+export default store
